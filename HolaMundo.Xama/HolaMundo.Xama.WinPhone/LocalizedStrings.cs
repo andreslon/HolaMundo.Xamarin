9178abd7e02e8ce463fe13bc1d30a3b4de5e8934
Andrés Londoño <andreslon@outlook.com>
2015-07-28 23:43:11 -0500

    Proyecto base de Hola Mundo

---
+++ HolaMundo.Xama/HolaMundo.Xama.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HolaMundo.Xama.WinPhone.Resources;
+
+namespace HolaMundo.Xama.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ HolaMundo.Xama/HolaMundo.Xama/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HolaMundo.Xama
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Result.Text = MyText.Text;
+        }
+    }
+}
